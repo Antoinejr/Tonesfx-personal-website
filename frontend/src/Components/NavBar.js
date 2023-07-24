06b357d36537086cea2e572a39e9f829e5881266
ANTO\pc-anto <anthony.irokosu@gmail.com>
2023-07-24 12:55:25 +0200

    incomplete-not interested in inital design

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "",
+    };
+  }
+
+  setActiveTab(selectedTab) {
+    this.setState({ active: selectedTab });
+  }
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li onClick={() => this.props.goToBio()}>Bio</li>
+          <li onClick={() => this.props.goToResume}>Resume</li>
+          <li onClick={() => this.props.goToPortfolio}>Portfolio</li>
+          <li onClick={() => this.props.goToContact}>Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+}
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,16 +1,47 @@
 import React from "react";
 import "./Style.css";
 import pdf from "../../assets/Resume.pdf";
+import NavBar from "../../Components/NavBar";
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    // Refs for different elements]
+    this.bioRef = React.createRef();
     this.resumeRef = React.createRef();
+    this.portfolioRef = React.createRef();
+    this.contactRef = React.createRef();
   }
 
-  scrollToResume() {
+  goToBio() {
+    this.bioRef.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "center",
+    });
+  }
+  goToResume() {
     if (this.resumeRef && this.resumeRef.current) {
       this.resumeRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+      });
+    }
+  }
+  goToPortfolio() {
+    if (this.portfolioRef && this.portfolioRef.current) {
+      this.portfolioRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+      });
+    }
+  }
+  goToContact() {
+    console.log("here");
+    if (this.contactRef && this.contactRef.current) {
+      this.contactRef.current.scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "center",
@@ -23,17 +54,23 @@ export default class Home extends React.Component {
       <div className="container">
         <div className="header">
           {/* anchors to different section in page */}
-          <h4 onClick={() => this.scrollToResume()}>Resume</h4>
+          {/* create a list item */}
+          <NavBar
+            goToBio={this.goToBio}
+            goToResume={this.goToResume}
+            goToPortfolio={this.goToPortfolio}
+            goToContact={this.goToContact}
+          />
         </div>
         <div className="body">
-          <div className="bio">
+          <div ref={this.bioRef} className="bio">
             <p className="aboutInfo">
               Hi My name is Anthony and i'm a Computer science student at
               University of Twente, I like to write programs and in my spare
               time do a bit of Photography and Writing
             </p>
           </div>
-          <div ref={(target) => (this.resumeRef = target)} className="resume">
+          <div ref={this.resumeRef} className="resume">
             <embed
               className="pdf"
               src={pdf}
@@ -43,9 +80,8 @@ export default class Home extends React.Component {
             />
           </div>
         </div>
-        <div className="gitRepo"></div>
-        <div className="imageCarousel"></div>
-        <div className="contact">
+        <div ref={this.portfolioRef} className="gitRepo"></div>
+        <div ref={this.contactRef} className="contact">
           <p>
             <a href="https://instagram.com/tones.fx">Instagram</a>
             <a href="https://github.com/Antoinejr">Github</a>
